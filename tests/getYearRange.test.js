8991a1c7729c93f392425bf0f0b2b17884ce8b51
Tim <timcpb@hotmail.com>
2021-03-18 22:12:40 +0000

    test getYearRange with mocked axios.get

---
+++ tests/getYearRange.test.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+const { getYearRange } = require("../src/utils/getYearRange");
+const mockedResponseData = require("../src/utils/__mocks__/oxfordYearsMockResponseData.json");
+
+describe("getWeatherData", () => {
+  // jest.mock("axios");
+
+  it("Successfully returns the range of years for Oxford", async () => {
+    const location = "oxford";
+
+    axios.get = jest.fn().mockResolvedValue({
+      data: mockedResponseData,
+    });
+
+    const yearRange = await getYearRange({ location: location });
+
+    expect(yearRange.startYear).toEqual(2015);
+  });
+});
