921e8aedabe53a51d3dfe92b745742c880234fb0
Tim <timcpb@hotmail.com>
2021-03-18 13:40:43 +0000

    set up project and install dependencies

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// Get maximum Temperature for a year - Must return a number
+exports.getMaxTemperature = async ({location, year}) => {
+	return 0;
+}
+
+// Get minimum temperature for a year - Must return a number
+exports.getMinTemperature = async ({location, year}) => {
+	return 0;
+}
+
+// Get maximum Temperature for all years - Must return a number
+exports.getMaxTemperatureForLocation = async ({location}) => {
+	return 0;
+}
+
+// Get minimum temperature for all years - Must return a number
+exports.getMinTemperatureForLocation = async ({location}) => {
+	return 0;
+}
+
+// Get average sun hours for a year - Must return a number
+exports.getAverageSunHours = async ({location, year}) => {
+	return 0;
+}
+
+// Get average sun hours for all years - Must return a number
+exports.getAverageSunHoursForLocation = async ({location}) => {
+	return 0;
+}
