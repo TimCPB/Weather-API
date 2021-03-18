4debec2b9b3b71271930933f0dff988caa20644f
Tim <timcpb@hotmail.com>
2021-03-18 20:20:33 +0000

    mock axios.get

--- tests/getMinTemperature.test.js
+++ tests/getMinTemperature.test.js
@@ -1,9 +1,16 @@
+const axios = require("axios");
+
 const { getMinTemperature } = require("../src/index");
+const mockedResponseData = require("../src/utils/__mocks__/oxford2018ResponseData.json");
 
 describe("getMinTemperature", () => {
   it("Successfully gets the min Temperature for oxford 2018", async () => {
     const location = "oxford";
     const year = 2018;
+
+    axios.get = jest.fn().mockResolvedValue({
+      data: mockedResponseData,
+    });
 
     const result = await getMinTemperature({ location: location, year: year });
 
