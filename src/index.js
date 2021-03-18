d4d27eefb5f1a44b6c0e0ce30ef74bd7fa3ce9be
Tim <timcpb@hotmail.com>
2021-03-18 15:21:54 +0000

    getMaxTemperature returns max temp for a given lcoationa and year

--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
+const axios = require("axios");
+
 // Get maximum Temperature for a year - Must return a number
-exports.getMaxTemperature = async ({location, year}) => {
-	return 0;
-}
+exports.getMaxTemperature = async ({ location, year }) => {
+  try {
+    const res = await axios.get(
+      `https://grudwxjpa2.execute-api.eu-west-2.amazonaws.com/dev/${location}/year/${year}`,
+      {
+        headers: {
+          "x-api-key": "mcDLmlxrtw7ZHC70gD8FL4rtrXSPsUEB4iSp4lg3",
+        },
+      }
+    );
+    const maxTemperaturesArray = res.data.result.map(
+      (month) => month.temperature_max
+    );
+    const maxTemperature = Math.max(...maxTemperaturesArray);
+    return maxTemperature;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // Get minimum temperature for a year - Must return a number
-exports.getMinTemperature = async ({location, year}) => {
-	return 0;
-}
+exports.getMinTemperature = async ({ location, year }) => {
+  return 0;
+};
 
 // Get maximum Temperature for all years - Must return a number
-exports.getMaxTemperatureForLocation = async ({location}) => {
-	return 0;
-}
+exports.getMaxTemperatureForLocation = async ({ location }) => {
+  return 0;
+};
 
 // Get minimum temperature for all years - Must return a number
-exports.getMinTemperatureForLocation = async ({location}) => {
-	return 0;
-}
+exports.getMinTemperatureForLocation = async ({ location }) => {
+  return 0;
+};
 
 // Get average sun hours for a year - Must return a number
-exports.getAverageSunHours = async ({location, year}) => {
-	return 0;
-}
+exports.getAverageSunHours = async ({ location, year }) => {
+  return 0;
+};
 
 // Get average sun hours for all years - Must return a number
-exports.getAverageSunHoursForLocation = async ({location}) => {
-	return 0;
-}
+exports.getAverageSunHoursForLocation = async ({ location }) => {
+  return 0;
+};
