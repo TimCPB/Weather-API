33a9ac9e4840b86dd963d4c5df90b40801eee367
Tim <timcpb@hotmail.com>
2021-03-19 17:39:13 +0000

    write test for getAverageSunHours - failing

---
+++ tests/getAverageSunHours.test.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+
+const { getAverageSunHours } = require("../src/index");
+const mockedResponseData = require("../src/utils/__mocks__/oxford2018MockResponseData.json");
+
+describe("getAverageSunHours", () => {
+  jest.mock("axios");
+
+  it("Successfully gets the average sun hours for Oxford 2018", async () => {
+    const location = "oxford";
+    const year = 2018;
+
+    axios.get = jest.fn().mockResolvedValue({
+      data: mockedResponseData,
+    });
+
+    const result = await getAverageSunHours({ location: location, year: year });
+
+    expect(result).toEqual(147.9);
+    jest.clearAllMocks();
+  });
+});
