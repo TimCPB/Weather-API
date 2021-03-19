6f826a3e2db78813fbffc0987a679a015e58cbb8
Tim <timcpb@hotmail.com>
2021-03-19 14:36:07 +0000

    new test for generateArray function

---
+++ tests/generateArray.test.js
@@ -0,0 +1,13 @@
+const { generateArray } = require("../src/utils/generateArray");
+
+describe("generateArray", () => {
+  it("Successfully returns an array of all numbers between a min and a max value, inclusive", () => {
+    const minValue = 5;
+    const maxValue = 9;
+
+    const expectedResult = [5, 6, 7, 8, 9];
+    const result = generateArray({ minimum: minValue, maximum: maxValue });
+
+    expect(result).toEqual(expectedResult);
+  });
+});
