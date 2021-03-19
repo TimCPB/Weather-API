388b72fcab148131553c21ba06e42a001311e60f
Tim <timcpb@hotmail.com>
2021-03-19 14:32:20 +0000

    skip tests for now, until mocks are added

--- tests/structure.test.js
+++ tests/structure.test.js
@@ -1,36 +1,38 @@
-const { getMaxTemperature, 
-	getMinTemperature, 
-	getMaxTemperatureForLocation, 
-	getMinTemperatureForLocation,
-	getAverageSunHours,
-	getAverageSunHoursForLocation } = require('../src/index')
+const {
+  getMaxTemperature,
+  getMinTemperature,
+  getMaxTemperatureForLocation,
+  getMinTemperatureForLocation,
+  getAverageSunHours,
+  getAverageSunHoursForLocation,
+} = require("../src/index");
 
-const LOCATION = 'oxford';
+const LOCATION = "oxford";
 const YEAR = 2018;
 
-describe('The exported function', ()=> {
-	it('getMaxTemperature returns a number', async () => {
-		const result = await getMaxTemperature({location: LOCATION, year: YEAR})
-		expect(typeof result).toEqual('number')
-	})
-	it('getMinTemperature returns a number', async () => {
-		const result = await getMinTemperature({location: LOCATION, year: YEAR})
-		expect(typeof result).toEqual('number')
-	})
-	it('getMaxTemperatureForLocation returns a number', async () => {
-		const result = await getMaxTemperatureForLocation({location: LOCATION})
-		expect(typeof result).toEqual('number')
-	})
-	it('getMinTemperatureForLocation returns a number', async () => {
-		const result = await getMinTemperatureForLocation({location: LOCATION})
-		expect(typeof result).toEqual('number')
-	})
-	it('getAverageSunHours returns a number', async () => {
-		const result = await getAverageSunHours({location: LOCATION, year: YEAR})
-		expect(typeof result).toEqual('number')
-	})
-	it('getAverageSunHoursForLocation returns a number', async () => {
-		const result = await getAverageSunHoursForLocation({location: LOCATION})
-		expect(typeof result).toEqual('number')
-	})
-})
+describe.skip("The exported function", () => {
+  it("getMaxTemperature returns a number", async () => {
+    const result = await getMaxTemperature({ location: LOCATION, year: YEAR });
+    expect(typeof result).toEqual("number");
+  });
+  it("getMinTemperature returns a number", async () => {
+    const result = await getMinTemperature({ location: LOCATION, year: YEAR });
+    expect(typeof result).toEqual("number");
+  });
+  it("getMaxTemperatureForLocation returns a number", async () => {
+    const result = await getMaxTemperatureForLocation({ location: LOCATION });
+    expect(typeof result).toEqual("number");
+  });
+  it("getMinTemperatureForLocation returns a number", async () => {
+    const result = await getMinTemperatureForLocation({ location: LOCATION });
+    expect(typeof result).toEqual("number");
+  });
+  it("getAverageSunHours returns a number", async () => {
+    const result = await getAverageSunHours({ location: LOCATION, year: YEAR });
+    expect(typeof result).toEqual("number");
+  });
+  it("getAverageSunHoursForLocation returns a number", async () => {
+    const result = await getAverageSunHoursForLocation({ location: LOCATION });
+    expect(typeof result).toEqual("number");
+  });
+});
