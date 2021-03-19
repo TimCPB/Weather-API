db73af188995926cffe03551c3679b028ffd7522
Tim <timcpb@hotmail.com>
2021-03-19 17:11:36 +0000

    create getMinElement() and pass corresponding test

---
+++ src/utils/getMinElement.js
@@ -0,0 +1,5 @@
+exports.getMinElement = ({ array }) => {
+  const minElement = Math.min(...array);
+
+  return minElement;
+};
---
+++ tests/getMinElement.test.js
@@ -0,0 +1,11 @@
+const { getMinElement } = require("../src/utils/getMinElement");
+
+describe("getMinElement", () => {
+  it("Returns the minimum element from an array of numbers", () => {
+    const array = [6, 0.7, 28, 15];
+
+    result = getMinElement({ array: array });
+
+    expect(result).toEqual(0.7);
+  });
+});
