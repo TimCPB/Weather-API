8280f2b277eccc2682ae3136e4f60841e3be8d32
Tim <timcpb@hotmail.com>
2021-03-19 23:30:24 +0000

    remove console.logs

--- src/utils/getArrayAverage.js
+++ src/utils/getArrayAverage.js
@@ -1,7 +1,5 @@
 exports.getArrayAverage = ({ array }) => {
   const nonNullArray = array.filter((e) => e != null);
-  console.log(nonNullArray);
   const result = nonNullArray.reduce((a, b) => a + b, 0) / nonNullArray.length;
-  console.log(result);
   return result;
 };
