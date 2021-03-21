558b1ebdf0bc6440302e9cd4979b801093421a07
Tim <timcpb@hotmail.com>
2021-03-21 18:06:49 +0000

    extra tests

--- src/getMaxTemp.js
+++ src/getMaxTemp.js
@@ -1,6 +1,8 @@
 const axios = require("axios");
 
 const { getArrayAverage } = require("./utils/getArrayAverage");
+const { roundToOneDecimalPlace } = require("./utils/roundToOneDecimalPlace");
+const { getAverageSunHours } = require("./index");
 
 getMaxTemperature = async ({ location, year }) => {
   try {
@@ -16,15 +18,24 @@ getMaxTemperature = async ({ location, year }) => {
     // console.log(resultsArray);
     const resultsArray = res.data.result.map((month) => month.temperature_max);
     const sunHoursArray = res.data.result.map((month) => month.sun);
-    console.log(res.data);
+    // console.log(res.data);
     // console.log(resultsArray);
     console.log(sunHoursArray);
     console.log(getArrayAverage({ array: sunHoursArray }));
     const maxTemp = Math.max(...resultsArray);
+
+    const result = await getAverageSunHours({ location: "oxford", year: 1903 });
+    console.log(`avg sun hours: ${result}`);
+
+    const result2 = roundToOneDecimalPlace({ number: 12.33333 });
+    const result3 = roundToOneDecimalPlace({ number: null });
+
+    console.log(`result of rounding legit number: ${result2}`);
+    console.log(`result of rounding null value: ${result3}`);
     // console.log(maxTemp);
   } catch (error) {
     console.log(error);
   }
 };
 
-getMaxTemperature({ location: "oxford", year: 2015 });
+getMaxTemperature({ location: "oxford", year: 1903 });
