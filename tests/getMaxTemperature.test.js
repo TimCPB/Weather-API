6030b18ac6ab6f240a2ffb31e771075a522d5769
Tim <timcpb@hotmail.com>
2021-03-21 22:30:45 +0000

    commented out error test

--- tests/getMaxTemperature.test.js
+++ tests/getMaxTemperature.test.js
@@ -4,9 +4,8 @@ const { getMaxTemperature } = require("../src/index");
 const mockedResponseData = require("../src/utils/__mocks__/oxford2018MockResponseData.json");
 
 describe("getMaxTemperature", () => {
-  jest.mock("axios");
-
   it("Successfully gets the max Temperature for oxford 2018", async () => {
+    jest.mock("axios");
     const location = "oxford";
     const year = 2018;
 
@@ -20,3 +19,16 @@ describe("getMaxTemperature", () => {
     jest.clearAllMocks();
   });
 });
+
+// describe("getMaxTemperature 2", () => {
+//   it("returns 0 in case of an error", async () => {
+//     const location = "oxford";
+//     const year = 1818;
+
+//     const result2 = await getMaxTemperature({ location: "oxford", year: 1818 });
+
+//     console.log(result2);
+
+//     expect(result2).toEqual(0);
+//   });
+// });
