7aa3a0d371ef9c46afbbc3855f6b0c6d35b7b4a7
Tim <timcpb@hotmail.com>
2021-03-21 16:01:31 +0000

    SRP - extract logic of getArrayAverage into private functions

--- src/utils/getArrayAverage.js
+++ src/utils/getArrayAverage.js
@@ -1,5 +1,14 @@
 exports.getArrayAverage = ({ array }) => {
-  const nonNullArray = array.filter((e) => e != null);
-  const result = nonNullArray.reduce((a, b) => a + b, 0) / nonNullArray.length;
-  return result;
+  const nonNullArray = _removeNullValues({ array: array });
+  const average = _returnAverage({ array: nonNullArray });
+
+  return nonNullArray.length > 0 ? average : null;
+};
+
+_removeNullValues = ({ array }) => {
+  return array.filter((e) => e != null);
+};
+
+_returnAverage = ({ array }) => {
+  return array.reduce((a, b) => a + b, 0) / array.length;
 };
