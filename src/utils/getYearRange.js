d8acf1b2bc9c7b062a6b431055839864976af5c6
Tim <timcpb@hotmail.com>
2021-03-21 22:32:39 +0000

    import env vars

--- src/utils/getYearRange.js
+++ src/utils/getYearRange.js
@@ -1,15 +1,14 @@
 const axios = require("axios");
 
+const { WEATHER_API_BASE_URL, WEATHER_API_KEY } = require("../config");
+
 exports.getYearRange = async ({ location }) => {
   try {
-    const res = await axios.get(
-      `https://grudwxjpa2.execute-api.eu-west-2.amazonaws.com/dev/${location}/years`,
-      {
-        headers: {
-          "x-api-key": "mcDLmlxrtw7ZHC70gD8FL4rtrXSPsUEB4iSp4lg3",
-        },
-      }
-    );
+    const res = await axios.get(`${WEATHER_API_BASE_URL}${location}/years`, {
+      headers: {
+        "x-api-key": `${WEATHER_API_KEY}`,
+      },
+    });
     return res.data.result;
   } catch (error) {
     console.log(error);
